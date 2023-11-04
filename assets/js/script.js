e529b2f3c2c73ebf02823988c1f6ee69f122bd47
David <130245859+DavidArtze@users.noreply.github.com>
2023-11-04 01:07:52 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+jQuery.noConflict();
+jQuery(document).ready(function($){
+								
+							
+function lightboxPhoto() {
+	
+	jQuery("a[data-gal^='prettyPhoto']").prettyPhoto({
+			animationSpeed:'fast',
+			slideshow:5000,
+			theme:'light_rounded',
+			show_title:false,
+			overlay_gallery: false
+		});
+	
+	}
+	
+		if(jQuery().prettyPhoto) {
+	
+		lightboxPhoto(); 
+			
+	}
+	
+	
+if (jQuery().quicksand) {
+
+ 	// Clone applications to get a second collection
+	var $data = $(".portfolio-area").clone();
+	
+	//NOTE: Only filter on the main portfolio page, not on the subcategory pages
+	$('.portfolio-categ li').click(function(e) {
+		$(".filter li").removeClass("active");	
+		// Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
+		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
+		
+		if (filterClass == 'all') {
+			var $filteredData = $data.find('.portfolio-item2');
+		} else {
+			var $filteredData = $data.find('.portfolio-item2[data-type=' + filterClass + ']');
+		}
+		$(".portfolio-area").quicksand($filteredData, {
+			duration: 600,
+			adjustHeight: 'auto'
+		}, function () {
+
+				lightboxPhoto();
+						});		
+		$(this).addClass("active"); 			
+		return false;
+	});
+	
+}//if quicksand
+
+});
+/*-------Google Map--------*/ 
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        var status = document.getElementById("status");
+        var data = new FormData(event.target);
+        fetch(event.target.action, {
+          method: form.method,
+          body: data,
+          headers: {
+              'Accept': 'application/json'
+          }
+        }).then(response => {
+          if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
+          } else {
+            response.json().then(data => {
+              if (Object.hasOwn(data, 'errors')) {
+                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+              } else {
+                status.innerHTML = "Oops! There was a problem submitting your form"
+              }
+            })
+          }
+        }).catch(error => {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        });
+      }
+      form.addEventListener("submit", handleSubmit)
